4aaebce84f53c2e3ca528b63b4ac27605b80be4b
Prince Bharti <bhartiprince.9971@gmail.com>
2018-07-26 16:00:41 +0530

    update to item model

---
+++ Todey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  item.swift
+//  Todey
+//
+//  Created by Prince Bharti on 26/07/18.
+//  Copyright © 2018 Prince Bharti. All rights reserved.
+//
+
+import Foundation
+
+class Item{
+    
+    var title = ""
+    var done = false
+
+}
